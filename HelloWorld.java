3dd6b4cfea29117f1f5898d22a30402ffe2e1480
yyds2333 <yangyadi1997@outlook.com>
2023-04-22 09:17:25 +0800

    add something

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		// 调用方法
+		one();
+	}
+
+	void one(){
+		System.out.println("This is my world!");
 	}
 }
